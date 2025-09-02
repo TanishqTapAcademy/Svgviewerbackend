f4ec744c4366a5d56fa7a0b9a39cad1ddfa42fb0
TanishqTapAcademy <tanishq@thetapacademy.com>
2025-09-02 15:02:37 +0530

    fronttend add production origin

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['*'], // Allow Vite dev server
-  credentials: true
+  origin: ['https://svgviewer-alpha.vercel.app', 'http://localhost:3000', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
